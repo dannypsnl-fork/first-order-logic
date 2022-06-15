65bae8ba4f94e0efc1b67fa6eb1abb2caacff912
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-06-15 21:35:16 +0800

    build term

--- src/main/scala/org/kmu/fol/BuildVisitor.scala
+++ src/main/scala/org/kmu/fol/BuildVisitor.scala
@@ -3,6 +3,13 @@ import org.kmu.fol.parser.{FolBaseVisitor, FolParser}
 import scala.jdk.CollectionConverters.*
 
 object BuildVisitor extends FolBaseVisitor[Logic]:
+  override def visitVariable(ctx: FolParser.VariableContext): Logic =
+    Variable(ctx.VAR().getText)
+  override def visitPredicate(ctx: FolParser.PredicateContext): Logic =
+    Predicate(
+      ctx.VAR().getText,
+      ctx.term().asScala.toSeq.map(x => visit(x).asInstanceOf[Term])
+    )
   override def visitWrap(ctx: FolParser.WrapContext): Logic = visit(ctx.logic())
   override def visitNot(ctx: FolParser.NotContext): Logic =
     Not(visit(ctx.logic()))
--- src/main/scala/org/kmu/fol/Logic.scala
+++ src/main/scala/org/kmu/fol/Logic.scala
@@ -1,6 +1,9 @@
 package org.kmu.fol
 
 trait Logic {}
+trait Term extends Logic {}
+case class Variable(name: String) extends Term
+case class Predicate(name: String, terms: Seq[Term]) extends Term
 case class Not(statement: Logic) extends Logic
 case class Or(subs: Seq[Logic]) extends Logic
 case class And(subs: Seq[Logic]) extends Logic
