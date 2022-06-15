d91a7dbfc73fa166567655b8843ed53614e3b503
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-06-15 22:08:56 +0800

    more tests

--- src/test/scala/org/kmu/fol/BuildVisitorSuite.scala
+++ src/test/scala/org/kmu/fol/BuildVisitorSuite.scala
@@ -13,10 +13,31 @@ class BuildVisitorSuite extends munit.FunSuite:
     val parser = parseString("X()")
     assertEquals(BuildVisitor.visit(parser.logic()), Constant("X"))
   }
+  test("predicate") {
+    val parser = parseString("D(x)")
+    assertEquals(
+      BuildVisitor.visit(parser.logic()),
+      Predicate("D", Variable("x"))
+    )
+  }
   test("and terms") {
     val parser = parseString("a and b")
     assertEquals(
       BuildVisitor.visit(parser.logic()),
       And(Variable("a"), Variable("b"))
+    )
+  }
+  test("or terms") {
+    val parser = parseString("A or B or C")
+    assertEquals(
+      BuildVisitor.visit(parser.logic()),
+      Or(Or(Variable("A"), Variable("B")), Variable("C"))
+    )
+  }
+  test("not terms") {
+    val parser = parseString("not not A")
+    assertEquals(
+      BuildVisitor.visit(parser.logic()),
+      Not(Not(Variable("A")))
     )
   }
