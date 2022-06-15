45887c4bc43d674b31f0b32fdac91385ba233298
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-06-15 22:02:24 +0800

    constant definition

--- src/main/scala/org/kmu/fol/Logic.scala
+++ src/main/scala/org/kmu/fol/Logic.scala
@@ -3,6 +3,7 @@ package org.kmu.fol
 trait Logic {}
 trait Term extends Logic {}
 case class Variable(name: String) extends Term
+case class Constant(name: String) extends Term
 case class Predicate(name: String, terms: Seq[Term]) extends Term
 case class Not(statement: Logic) extends Logic
 case class Or(subs: Seq[Logic]) extends Logic
